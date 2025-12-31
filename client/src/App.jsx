324aef266de45bf8b1325931c6339f6b8e7dc504
siddhikharde <siddhikharde2005@gmail.com>
2025-12-31 19:23:34 +0530

    data fectched from db

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [bucketList, setBucketList]=useState([]);
+  const loadBucketList=async ()=>{
+    const response=await axios.get(
+      `${import.meta.env.VITE_BASE_URL}/bucketlist`
+    )
+    setBucketList(response.data.data);
+  }
+
+  useEffect(()=>{
+    loadBucketList();
+  },[]);
   return (
     <div>
-      
+      {
+        bucketList.map((listItem)=>{
+          const {_id,name, discription,isCompleted}=listItem;
+         return<div key={_id}>
+          <h1><span>{isCompleted?"✅":"⏳" }</span>{name}</h1>
+          <p>{discription}</p>
+         </div>
+        })
+      }
     </div>
   )
 }
